c2e92622a753b0b19073e48051d10ff918b28297
mdaltoon1028 <stinnettkeli4@gmail.com>
2026-07-02 01:08:33 +0330

    fix: update backup file extension to .json
    Change the database backup filename from .db to .json to accurately reflect the exported file format.

--- server.ts
+++ server.ts
@@ -5308,7 +5308,7 @@ app.get("/api/backup-download", (req, res) => {
 
     res.setHeader(
       "Content-Disposition",
-      "attachment; filename=Daltoon_Bot.db",
+      "attachment; filename=Daltoon_Backup.json",
     );
     res.setHeader("Content-Type", "application/json");
     res.send(JSON.stringify(db, null, 2));
